b785f517c8035197423fd79f0a6a9c6b84c78fa5
fjmg15 <fjmg.1@icloud.com>
2025-04-30 17:07:31 -0600

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Cálculo de interés simple
+
+echo "Ingrese el capital:"
+read principal
+echo "Ingrese la tasa de interés (en %):"
+read rate
+echo "Ingrese el tiempo (en años):"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+echo "El interés simple es: $interest"
